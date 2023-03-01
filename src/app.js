ad922ba692a8be0a8aed834b38ccecd4c4c94c56
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2023-03-01 16:07:32 +0300

    Değişkenleri ve console.log, for gösterdi

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 console.log("Kammile Hoş Geldiniz");
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+{
+    let dolarDun = 9.10;
+}
+console.log(dolarDun);
+
+const euroDun = 11.2;
+//euroDun = 11;
+
+console.log(euroDun);
+
+//array
+//camelCasing
+//pascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"];
+//console.log(konutKredileri);
+
+console.log("<ul>");
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>");
+}
+console.log("</ul>");
