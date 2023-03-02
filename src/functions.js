45d9f12a231b91930f7a8727df57060d367a41d5
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2023-03-02 16:33:05 +0300

    Obje Özellikleri

--- src/functions.js
+++ src/functions.js
@@ -43,4 +43,53 @@ let sayi1 = 10;
 let sayi2 = 20;
 sayi1 = sayi2;
 sayi2 = 100;
-console.log(sayi1)
+console.log(sayi1);
+
+function addToCard4(products) {
+    console.log(products);
+}
+
+let products = [
+    {producName:"Limon", quantity:14, unitPrice:15},
+    {producName:"Nane", quantity:11, unitPrice:25},
+    {producName:"Armut", quantity:5, unitPrice:10}
+]
+
+addToCard4(products);
+
+function add(...numbers) {
+    let total = 0; 
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i];
+    }
+    console.log(total);
+}
+//add(20,30);
+//add(20,30,40);
+add(20,30,40,50);
+
+let numbers = [30,50,600,750,90,110]
+//console.log(...numbers)
+console.log(Math.max(...numbers))
+
+let [icAnadolu, marmara, karadeniz, [icAnadoluSehirleri]] = [
+    {name:"İç Anadolu", population:"20M"},
+    {name:"Marmara", population:"40M"},
+    {name:"Karadeniz", population:"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Samsun","Trabzon"]
+    ]
+]
+console.log(icAnadolu.name);
+console.log(icAnadolu.population);
+console.log(icAnadoluSehirleri);
+
+let newProductName, newUnitPrice, newQuantity
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity}
+= {productName:"Elma", unitPrice:10, quantity: 15})
+
+console.log(newProductName);
+console.log(newUnitPrice);
+console.log(newQuantity);
