0fcc48d2009544e864150b5bb3c10e7d8202c71b
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2023-03-03 16:10:31 +0300

    Class Kullanımı

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,12 +1,17 @@
+import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logger.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js"
 
 console.log("User Component Eklendi")
 
-let userService = new UserService()
+let logger1 = new ElasticLogger()
+let userService = new UserService(logger1)
 
 let user1 = new User(1, "Ali Rıza", "Yüksel", "İstanbul")
-userService.add(user1.firstName)
+let user2 = new User(2, "Rıdvan", "Aydın", "Giresun")
+userService.add(user1)
+userService.add(user2)
+console.log(userService.list())
 
 userService.add();
 userService.getById(1);
